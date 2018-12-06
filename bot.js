668e0a6aa4eb6e77451a199411ffc4f78bc7aa02
TheSpammer <45629345+TheSpammer@users.noreply.github.com>
2018-12-06 13:22:05 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('BOT STARTED UP!');
+});
+
+client.on('ready', () => {
+  client.user.setActivity("help l shows help page.", {url: "https://www.twitch.tv"});
+  console.log('${client.user.tag} running on ${client.guilds.size} guilds with ${client.users.size} users.');
+});
+
+
+client.on('message', async msg=> {
+  if (msg.content === 'open') {
+	  msg.channel.send('open')
+  }
+  if (msg.content === 'stop') {
+	  msg.channel.send('')
+  }
+});
+
+
+
+client.on('message', async msg=> {
+  if (msg.content === 'credits') {
+      msg.channel.send('#credits <@519838438414090244>')
+  }
+});
+
+
+client.on('message', async msg=> {
+  if (msg.content === 'give me') {
+      msg.channel.send('#credits <@453683156294434817> 500')
+  }
+});
+
+client.on('message', async msg=> {
+  if (msg.content === 'daily') {
+      msg.channel.send('#daily')
+  }
+});
+
+
+client.login('NTE5ODM4NDM4NDE0MDkwMjQ0.DulSFg.eptcw3cyFD-jVFCDVMZwzsTOgbg');
